0d84217c3b5be09b6be9d902a0f7a86edd433311
acf879 <55860949+acf879@users.noreply.github.com>
2021-03-20 16:27:05 -0600

    Bug fixes
    Fixed bug with spaces between operations

--- main.py
+++ main.py
@@ -24,50 +24,59 @@ class C1:
         pass
 
     def eval1(self):
-        _func = self._func
-        _func = _func.replace(self._var, str(self._value))
+        _func_raw = self._func.replace(' ', '')
+        _func_data = _func_raw.replace(self._var, str(self._value))
+        _func_data.strip()
+        _func_lst = _func_data.split('(')
 
-        while '^' in _func:
-            left_ind = _func.index('^') - 1
-            right_ind = _func.index('^') + 1
-            left_val = int(_func[left_ind])
-            right_val = int(_func[right_ind])
-            _func = _func[:left_ind] + str(self.power(left_val, right_val)) + _func[right_ind + 1:]
+        if ' ' in _func_lst:
+            _func_lst.pop(_func_lst.index(' '))
+        if '' in _func_lst:
+            _func_lst.pop(_func_lst.index(''))
 
-        while 'rt' in _func:
-            left_ind = _func.index('rt') - 1
-            right_ind = _func.index('rt') + 2
-            left_val = int(_func[left_ind])
-            right_val = int(_func[right_ind])
-            _func = _func[:left_ind] + str(self.root(left_val, right_val)) + _func[right_ind + 1:]
+        for _func in _func_lst:
+            while '^' in _func:
+                left_ind = _func.index('^') - 1
+                right_ind = _func.index('^') + 1
+                left_val = int(_func[left_ind])
+                right_val = int(_func[right_ind])
+                _func = _func[:left_ind] + str(self.power(left_val, right_val)) + _func[right_ind + 1:]
 
-        while '/' in _func:
-            left_ind = _func.index('/') - 1
-            right_ind = _func.index('/') + 1
-            left_val = int(_func[left_ind])
-            right_val = int(_func[right_ind])
-            _func = _func[:left_ind] + str(self.div(left_val, right_val)) + _func[right_ind + 1:]
+            while 'rt' in _func:
+                left_ind = _func.index('rt') - 1
+                right_ind = _func.index('rt') + 2
+                left_val = int(_func[left_ind])
+                right_val = int(_func[right_ind])
+                _func = _func[:left_ind] + str(self.root(left_val, right_val)) + _func[right_ind + 1:]
 
-        while '*' in _func:
-            left_ind = _func.index('*') - 1
-            right_ind = _func.index('*') + 1
-            left_val = int(_func[left_ind])
-            right_val = int(_func[right_ind])
-            _func = _func[:left_ind] + str(self.mult(left_val, right_val)) + _func[right_ind+1:]
+            while '/' in _func:
+                left_ind = _func.index('/') - 1
+                right_ind = _func.index('/') + 1
+                left_val = int(_func[left_ind])
+                right_val = int(_func[right_ind])
+                _func = _func[:left_ind] + str(self.div(left_val, right_val)) + _func[right_ind + 1:]
 
-        while '+' in _func:
-            left_ind = _func.index('+') - 1
-            right_ind = _func.index('+') + 1
-            left_val = int(_func[left_ind])
-            right_val = int(_func[right_ind])
-            _func = _func[:left_ind] + str(self.add(left_val, right_val)) + _func[right_ind + 1:]
+            while '*' in _func:
+                left_ind = _func.index('*') - 1
+                right_ind = _func.index('*') + 1
+                left_val = int(_func[left_ind])
+                right_val = int(_func[right_ind])
+                _func = _func[:left_ind] + str(self.mult(left_val, right_val)) + _func[right_ind+1:]
 
-        while '-' in _func:
-            left_ind = _func.index('-') - 1
-            right_ind = _func.index('-') + 1
-            left_val = int(_func[left_ind])
-            right_val = int(_func[right_ind])
-            _func = _func[:left_ind] + str(self.sub(left_val, right_val)) + _func[right_ind + 1:]
+            while '+' in _func:
+                print(_func)
+                left_ind = _func.index('+') - 1
+                right_ind = _func.index('+') + 1
+                left_val = int(_func[left_ind])
+                right_val = int(_func[right_ind])
+                _func = _func[:left_ind] + str(self.add(left_val, right_val)) + _func[right_ind + 1:]
+
+            while '-' in _func:
+                left_ind = _func.index('-') - 1
+                right_ind = _func.index('-') + 1
+                left_val = int(_func[left_ind])
+                right_val = int(_func[right_ind])
+                _func = _func[:left_ind] + str(self.sub(left_val, right_val)) + _func[right_ind + 1:]
 
         print(_func)
 
@@ -90,6 +99,6 @@ class C1:
         return var1**var2
 
 
-c = C1('xrt2','x',2)
+c = C1('xrt2 + 2', 'x', 2)
 c.eval1()
 
